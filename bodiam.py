ec79b4a636d8a09a51a68a141a929f61fa6c0847
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2021-10-29 13:34:03 -0700

    Create bodiam.py

---
+++ bodiam.py
@@ -0,0 +1,13 @@
+###
+### Author: Xin Li
+### Description: you will print out
+### ASCII-art of the Bodiam castle.
+
+width = int(input('castle width:\n'))
+print(' ')
+print(' '*width+'   .-.-.')
+print('[-]'+' '*width+'|-.-|'+' '*width+'[-]')
+print('| |'+'_'*width+'| H |'+'_'*width+'| |')
+print('| |'+' '*width+'| H |'+' '*width+'| |')
+print('| |'+' '*width+'| H |'+' '*width+'| |')
+print('|_|'+'_'*width+'||^||'+'_'*width+'|_|')
